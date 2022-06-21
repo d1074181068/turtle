cf58a523d916a3b0c212d59474fc68b7d832863f
d1074181068 <wiwi19991214@gmail.com>
2022-06-21 21:16:17 +0800

    add totop_animate

--- javascripts/animate.js
+++ javascripts/animate.js
@@ -12,7 +12,12 @@ $(document).ready(function () {
 
     });
 
-
+    $('.to_top a').click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
     function showBtnCondition() {
         if ($(this).scrollTop() > 700) {
             $('.to_top').fadeIn();
